99a84bb34a8d8b7bf9afaf8438145a9335374e3c
Mathieu <vinot.mathieu88@gmail.com>
2023-04-13 16:54:05 +0200

    Tests grammaire

---
+++ src/test/java/TestExemple5.java
@@ -0,0 +1,32 @@
+import generated.fr.ul.miage.ahvr.projetCompilationComplet.ParserCup;
+import generated.fr.ul.miage.ahvr.projetCompilationComplet.Yylex;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class TestExemple5 {
+    @Before
+    public void before(){
+
+    }
+
+    @Test
+    public void test() {
+        try {
+            File file = new File("src/test/resources/exemple5.ahvr");
+            Yylex scanner = new Yylex(new FileReader(file));
+            ParserCup parser = new ParserCup(scanner);
+            parser.parse();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @After
+    public void after(){
+
+    }
+}
