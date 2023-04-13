ce4553b8bd5dde438bc53a9985bae1620eb4bd4d
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2023-04-13 19:05:51 +0200

    Main fonctionnel

--- src/main/java/fr/ul/miage/ahvr/projetCompilationComplet/Main.java
+++ src/main/java/fr/ul/miage/ahvr/projetCompilationComplet/Main.java
@@ -1,6 +1,9 @@
 package fr.ul.miage.ahvr.projetCompilationComplet;
 
 import java.io.FileReader;
+import Generateur.Generateur;
+import generated.fr.ul.miage.ahvr.projetCompilationComplet.ParserCup;
+import generated.fr.ul.miage.ahvr.projetCompilationComplet.Yylex;
 
 public class Main {
     public static void main(String[] args){
@@ -9,9 +12,10 @@ public class Main {
             System.exit(1);
         }
         try {
-//            Yylex scanner = new Yylex(new FileReader(args[0]));
-//            ParserCup parser = new ParserCup(scanner);
-//            parser.parse();
+            Yylex scanner = new Yylex(new FileReader(args[0]));
+            ParserCup parser = new ParserCup(scanner);
+            parser.parse();
+            System.out.println(Generateur.genererProgramme(parser.resultat, parser.tds));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
