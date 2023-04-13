320669e1dad823aa351e343396c5d5cadb512255
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2023-04-13 17:36:40 +0200

    Exemple 2 marche

--- src/test/java/TestExemple2.java
+++ src/test/java/TestExemple2.java
@@ -1,3 +1,4 @@
+import fr.ul.miage.arbre.TxtAfficheur;
 import generated.fr.ul.miage.ahvr.projetCompilationComplet.ParserCup;
 import generated.fr.ul.miage.ahvr.projetCompilationComplet.Yylex;
 import org.junit.After;
@@ -21,6 +22,8 @@ public class TestExemple2 {
             Yylex scanner = new Yylex(new FileReader(file));
             ParserCup parser = new ParserCup(scanner);
             parser.parse();
+            TxtAfficheur.afficher(parser.resultat);
+            System.out.println(parser.tds);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
